e29bb7f5a050441a5a4be34cac70bfd9878475e7
Artem Khusnullin <twentyfortythree@yandex.ru>
2023-07-06 23:53:22 +0500

    Тестовый пуш

--- app.js
+++ app.js
@@ -31,9 +31,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.post('/signin', login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    password: Joi.string(),
-    about: Joi.string().required().min(2).max(30),
+    name: Joi.string().min(2).max(30),
+    password: Joi.string().required(),
+    about: Joi.string().min(2).max(30),
     email: Joi.string().email().required(),
     avatar: Joi.string().pattern(/https?:\/\/[www]*[a-z0-9-._~:/?#\\@!$&'()*+,;=]+[.ru]*/),
   }),

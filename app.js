3ceaf43d7801f09cf25dbe37e161ea58b122319c
Artem Khusnullin <twentyfortythree@yandex.ru>
2023-07-18 16:54:47 +0500

    Внёс правки ПР15

--- app.js
+++ app.js
@@ -34,8 +34,9 @@ app.use((req, res, next) => {
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
-
+  const { origin } = req.headers;
   if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', origin)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();

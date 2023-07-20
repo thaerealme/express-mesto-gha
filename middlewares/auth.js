18e2579dcdf4851ca55254c04923b39cda2a892a
Artem Khusnullin <twentyfortythree@yandex.ru>
2023-07-20 15:39:50 +0500

    Пуш ласт

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, process.env.JWT_SECRET);
+    payload = jwt.verify(token, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new AuthError('Необходимо авторизоваться!'));
     return;
